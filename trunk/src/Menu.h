fd8aeefb98ca6ed1b6945b4c3949050a43cd2e59
steven.durrenmatt@gmail.com <steven.durrenmatt@gmail.com@41cf5c61-3d20-4462-bb72-7601b4cbf867>
2013-03-28 14:49:13 +0000

    Création du menu

---
+++ trunk/src/Menu.h
@@ -0,0 +1,79 @@
+#ifndef _MENU
+#define _MENU
+
+/**
+
+ \file Menu.h
+ \brief Module Menu.h
+ \author Steven Durrenmath
+
+ */
+
+
+typedef void (*MenuFonction)();
+
+
+/** \struct MenuLigne */
+typedef struct
+{
+	char texte[64];
+	MenuFonction commande;
+} MenuLigne;
+
+
+/** \struct Menu */
+typedef struct
+{
+	MenuLigne lignes[32];
+	int nb_lignes;
+} Menu;
+
+
+/**
+
+ \fn void menuInit(Menu *m)
+ \brief Initialise un menu
+
+ \param[in, out] m Pointeur sur un menu
+
+*/
+void menuInit(Menu *m);
+
+
+/**
+
+ \fn void menuAjouterLigne(Menu *m, char texte[64], MenuFonction fonction);
+ \brief Ajoute une ligne dans un menu
+
+ \param[in, out] m Pointeur sur un menu
+ \param[in] texte Nom de la nouvelle ligne de menu
+ \param[in] fonction Fonction associée à la ligne
+
+*/
+void menuAjouterLigne(Menu *m, char texte[64], MenuFonction fonction);
+
+
+/**
+
+ \fn menuBoucle(Menu *m)
+ \brief Affiche le menu en boucle
+
+ \param[in, out] Pointeur sur un menu
+
+*/
+void menuBoucle(Menu *m);
+
+
+/**
+
+ \fn void menuLibere(Menu *m)
+ \brief Libère un menu
+
+ \param[in, out] m Pointeur sur un menu
+
+*/
+void menuLibere(Menu *m);
+
+
+
+#endif
